32ffd5ae70c65d9b3a248996822de5d20dfc862d
Mangesh Rane <mrane>
2019-05-10 18:15:51 +0530

    first commit

---
+++ SeleniumWebdriver/FirstTest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace SeleniumWebdriver
+{
+    [TestClass]
+    class FirstTest
+    {
+        [TestMethod]
+        public void test()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "http://google.com";
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            element.SendKeys("Test" + Keys.Enter);
+        }
+        
+    }
+}
---
+++ WebdriverTest/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebdriverTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static IWebDriver driver;
+
+        // can be used to provide medadata about the 
+        // gives runtime details about testcases
+        public TestContext TestContext { get; set; }
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            
+            Console.WriteLine("Assembly initialize method will run before any of the assembly will run before any test method in assembly");
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            Console.WriteLine("Assembly Cleanup will run after the test in Assembly");
+        }
+
+        [ClassInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driver = new ChromeDriver();
+            driver.Url = "http://google.com";
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Console.WriteLine("Test Initialize ran");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Console.WriteLine("Test Cleanup ran");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestContext.WriteLine("TestRun directoty: {0}",
+                TestContext.TestRunDirectory);
+            TestContext.WriteLine("TestName: {0} ", TestContext.TestName);
+            TestContext.WriteLine("Test Outcome: {0}", TestContext.CurrentTestOutcome);
+
+            Console.WriteLine("test Method");
+        }
+      
+        [TestMethod]
+        public void TestMethod()
+        {
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("Test" + Keys.Enter);
+            Assert.IsTrue(driver.Title.StartsWith("Test"));
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
